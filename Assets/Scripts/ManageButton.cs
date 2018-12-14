e776c5a30e481608f054cf4cd1451e2209c98f3a
hironow <hironow365@gmail.com>
2018-12-14 22:38:45 +0900

    add sign-in native plugin(FirebaseAuth, GoogleSignIn)

---
+++ Assets/Scripts/ManageButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageButton : MonoBehaviour
+{
+    private Text label;
+
+    void Start() {
+        label = this.gameObject.GetComponentInChildren<Text>();
+        
+        label.text = label.text == "SignIn" ? "SignOut" : "SignIn";
+        
+        // IDToken取得
+        // 取得できれば SignOut 、できなければ SingIn を表示する
+        FirebaseAuthPlugIn.CallPlugin(OnEndOfGetIDToken);
+
+    }
+
+    public void OnClickButton() {
+        if (label.text == "SignIn") {
+            // SignIn実行
+            ManageScroll.Log(DateTime.Now.ToString("tthh時mm分ss秒fffミリ秒") + ": SignIn");
+            GoogleSignInPlugIn.SignIn();
+            label.text = "SignOut";
+        } else {
+            // SignOut実行
+            ManageScroll.Log(DateTime.Now.ToString("tthh時mm分ss秒fffミリ秒") + ": SignOut");
+            GoogleSignInPlugIn.SignOut();
+            label.text = "SignIn";
+        }
+    }
+    
+    private void OnEndOfGetIDToken(string token, string err) {
+        Debug.Log("token: " + token);
+        Debug.Log("err: " + err);
+
+        if (err == "" && token != "") {
+            // SingIn済み
+            ManageScroll.Log("token: " + token);
+            label.text = "SignOut";
+        }
+
+        if (err != "") {
+            ManageScroll.Log("err: " + err);
+            label.text = "SignIn";
+        }        
+    }
+}
